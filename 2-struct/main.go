bca3878f0183cfec5dd9a4e4e271e04cb1ae9f39
Rick Houser <21@p6-4.hbtn.io>
2016-03-05 18:54:13 -0800

    change script to calculate by seconds

--- 2-struct/main.go
+++ 2-struct/main.go
@@ -13,8 +13,12 @@ func main() {
 	form := "January 2, 2006"
 	u := user{Name: "Betty Holberton", DOB: "March 7, 1917", City: "Philadelphia"}
 	fmt.Printf("Hello %s\n", u.Name)
-	t, _ := time.Parse(form, u.DOB)
-	r := time.Now()
-	result := r.Year() - t.Year()
-	fmt.Printf("%s was born in %s would be %d years old today.\n", u.Name, u.City, result)
+	t, err := time.Parse(form, u.DOB)
+	result := time.Since(t).Seconds() / 31557600
+
+	if err != nil {
+		fmt.Printf("error %v", err)
+		return
+	}
+	fmt.Printf("%s was born in %s would be %d years old today.\n", u.Name, u.City, int(result))
 }
